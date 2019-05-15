a11551e727aae56a08b54315c48e0ef1213de013
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-05-16 06:10:49 +0900

    rolling Completed

--- src/BlackBoard.js
+++ src/BlackBoard.js
@@ -1,22 +1,96 @@
-import React from 'react';
-
-
-
+import React, { useState } from "react";
+import { useSpring, animated } from "react-spring";
 
 function BlackBoard(props) {
-  
+  const prop0 = useSpring({
+    from: {
+      opacity: 0,
+      fontSize: "12px",
+      color: "#fff",
+      padding: "0px auto",
+      margin: "0 auto",
+      textAlign: "center"
+    },
+    to: {
+      opacity: 0.5,
+      fontSize: "24px",
+      color: "#fff",
+      padding: "0px auto",
+      textAlign: "center"
+    },
+    config: { duration: 500 },
+    reset: true
+  });
+
+  const prop1 = useSpring({
+    from: {
+      opacity: 0.5,
+      fontSize: "24px",
+      color: "#fff",
+      margin: "0px auto",
+      textAlign: "center"
+    },
+    to: {
+      opacity: 1,
+      fontSize: "48px",
+      color: "#fff",
+      margin: "0px auto",
+      textAlign: "center"
+    },
+    config: { duration: 500 },
+    reset: true
+  });
+
+  const prop2 = useSpring({
+    from: {
+      opacity: 1,
+      fontSize: "40px",
+      color: "#fff",
+      margin: "20px auto",
+      textAlign: "center"
+    },
+    to: {
+      opacity: 0.5,
+      fontSize: "24px",
+      color: "#fff",
+      margin: "20px auto",
+      textAlign: "center"
+    },
+    config: { duration: 500 },
+    reset: true
+  });
+
+  const exercises = [
+    "",
+    "1 + 1 = ?",
+    "2 + 1 = ?",
+    "3 + 1 = ?",
+    "4 + 1 = ?",
+    "5 + 1 = ?",
+    "6 + 1 = ?",
+    "7 + 1 = ?",
+    ""
+  ];
+
+  const [count, setCount] = useState(0);
+  const itemChange = () => {
+    setCount(count + 1);
+  };
+
   return (
-    <div className='black-board'>
-      <p className={props.classes[0].element}>{props.questions[3].question}</p>
-      <p className={props.classes[1].element}>{props.questions[2].question}</p>
-      <p 
-        id='temp'
-        className={props.classes[2].element} 
-        onClick={() => props.moveElements(props.classes)}
-      >{props.questions[1].question}</p>
-      <p className={props.classes[3].element}>{props.questions[0].question}</p>
+    <div
+      className="black-board"
+      onClick={() => {
+        itemChange();
+      }}
+    >
+      <div className="math-content">
+        <animated.div style={prop0}>{exercises[count + 2]}</animated.div>,
+        <animated.div style={prop1}>{exercises[count + 1]}</animated.div>,
+        <animated.div style={prop2}>{exercises[count + 0]}</animated.div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default BlackBoard;
